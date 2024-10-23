65b620ab365d402a222e5a7099dff626982e1864
João Bertholino <joaobertholino964@gmail.com>
2024-10-23 19:36:44 -0300

    feat: Spring project base.

---
+++ src/main/java/dev/joaobertholino/challengecurrencyconverter/ChallengeCurrencyConverterApplication.java
@@ -0,0 +1,13 @@
+package dev.joaobertholino.challengecurrencyconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeCurrencyConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeCurrencyConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joaobertholino/challengecurrencyconverter/ChallengeCurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaobertholino.challengecurrencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeCurrencyConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
